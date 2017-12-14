d9b8b28cb45c2c57131bd1fd2f9e76f110a08c56
Takafumi Nakahara <kodawaripoteto@gmail.com>
2017-12-15 05:16:54 +0900

    ソフトウェアキーボードのGoを押したときにキーボードが閉じるように設定

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,4 +5,15 @@ $(function() {
     $('#wrapper').addClass('show');
   });
 
+  window.my = {};
+  window.my.onkeypress = function() {
+      var ENTER_KEY_CODE = 13;
+      // ENTERが押下された場合、ソフトウェアキーボードを閉じる
+      if (event.keyCode == ENTER_KEY_CODE) {
+          document.activeElement.blur()
+      }
+      // ENTERが押下された時はfalseを返す。理由はPOSTリクエストを送信させないため。
+      return event.keyCode != ENTER_KEY_CODE;
+  }
+
 });
