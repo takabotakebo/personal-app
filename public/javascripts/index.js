2734d6dbc0c4f1cfdeee4a1b2c38b2cbe3191fc9
Takafumi Nakahara <kodawaripoteto@gmail.com>
2017-12-17 10:28:28 +0900

    再生停止実装

---
+++ public/javascripts/index.js
@@ -0,0 +1,63 @@
+// APIにPOSTリクエストを送る
+function postDataStop(){
+  console.log("Send Data as POST");
+
+  //フォームの内容を取得する処理
+  var answer = 0;
+  console.log("Answer is:" + answer);
+
+  $.ajax(
+    {
+      url: "/",
+      type:'POST',
+      dataType: 'json',
+      data : {
+        data : answer
+      },
+      timeout:10000,
+    }
+  ).done(
+    function(data) {
+      console.log("It works");
+      $('.stopbutton').hide(50);
+      $('.startbutton').show(50);
+
+    }
+  ).fail(
+    function(XMLHttpRequest, textStatus, errorThrown) {
+      window.location.href = '/errorpage';
+    }
+  )
+
+}
+
+function postDataStart(){
+  console.log("Send Data as POST");
+
+  //フォームの内容を取得する処理
+  var answer = 1;
+  console.log("Answer is:" + answer);
+
+  $.ajax(
+    {
+      url: "/",
+      type:'POST',
+      dataType: 'json',
+      data : {
+        data : answer
+      },
+      timeout:10000,
+    }
+  ).done(
+    function(data) {
+      console.log("It works");
+      $('.startbutton').hide(50);
+      $('.stopbutton').show(50);
+    }
+  ).fail(
+    function(XMLHttpRequest, textStatus, errorThrown) {
+      window.location.href = '/errorpage';
+    }
+  )
+
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const osc = require('node-osc');//OSC のインポート
+
+//IPアドレスの指定
+var ipAdrress = "localhost";
+//OSCの送信先サーバを定義(4000番ポート)
+var send_osc = new osc.Client(ipAdrress, 4000);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.post('/', function(req, res, next) {
+  var data = req.body.data;
+
+  //データを送信
+  send_osc.send('/startstop',Number(data));
+
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  res.send(data);
+
+});
 module.exports = router;
