be681c0c04e6756e9cfddba29397211d1bf17366
TakafumiNakahara <kodawaripoteto@gmail.com>
2018-03-03 19:46:54 +0900

    スキップ機能の実装

--- public/javascripts/height.js
+++ public/javascripts/height.js
@@ -38,3 +38,41 @@ function postData(){
   )
 
 }
+
+// スキップの場合
+// APIにPOSTリクエストを送る
+function postDataSkip(){
+  console.log("Send Data as POST");
+
+  console.log("Answer is: SKIP");
+
+  $.ajax(
+    {
+      url: "/height",
+      type:'POST',
+      dataType: 'json',
+      data : {
+        gender : localStorage.getItem("gender"),
+        height : localStorage.getItem("height"),
+        weight : localStorage.getItem("weight"),
+        born_y : localStorage.getItem("born_y"),
+        born_m : localStorage.getItem("born_m"),
+        born_d : localStorage.getItem("born_d"),
+        firstname : localStorage.getItem("firstname"),
+        familyname : localStorage.getItem("familyname"),
+        createdAt : localStorage.getItem("createdAt")
+      },
+      timeout:10000,
+    }
+  ).done(
+    function(data) {
+      console.log("It works");
+      window.location.href = '/weight';
+    }
+  ).fail(
+    function(XMLHttpRequest, textStatus, errorThrown) {
+      window.location.href = '/errorpage';
+    }
+  )
+
+}
--- public/javascripts/name.js
+++ public/javascripts/name.js
@@ -44,3 +44,41 @@ function postData(){
   )
 
 }
+
+
+// APIにPOSTリクエストを送る
+function postDataSkip(){
+  console.log("Send Data as POST");
+
+  console.log("Answer is: SKIP");
+
+  $.ajax(
+    {
+      url: "/name",
+      type:'POST',
+      dataType: 'json',
+      data : {
+        gender : localStorage.getItem("gender"),
+        height : localStorage.getItem("height"),
+        weight : localStorage.getItem("weight"),
+        born_y : localStorage.getItem("born_y"),
+        born_m : localStorage.getItem("born_m"),
+        born_d : localStorage.getItem("born_d"),
+        firstname : localStorage.getItem("firstname"),
+        familyname : localStorage.getItem("familyname"),
+        createdAt : localStorage.getItem("createdAt")
+      },
+      timeout:10000,
+    }
+  ).done(
+    function(data) {
+      console.log("It works");
+      window.location.href = '/';
+    }
+  ).fail(
+    function(XMLHttpRequest, textStatus, errorThrown) {
+      window.location.href = '/errorpage';
+    }
+  )
+
+}
